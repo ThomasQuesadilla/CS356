ea71f3dc820709ff61bebe21ecac9ddf66faca59
kyratichan <54815433+kyratichan@users.noreply.github.com>
2021-09-21 13:07:51 -0400

    Update Group3_UDPHeartbeatServer.py

--- Group3_UDPHeartbeatServer.py
+++ Group3_UDPHeartbeatServer.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 18 14:05:58 2021
-
-@author: kyrachan
-"""
-
-# UDPPingerServer.py
-# We will need the following modules to generate randomized lost packets
 import random
 from socket import *
 import time
@@ -15,29 +5,44 @@ import time
 # Create a UDP socket
 # Notice the use of SOCK_DGRAM for UDP packets
 serverSocket = socket(AF_INET, SOCK_DGRAM)
+# Set timeout to 5s
 serverSocket.settimeout(5.0)
 
 # Assign IP address and port number to socket
 serverSocket.bind(('localhost', 12000))
+# Keep track of expected SeqNum to check for lost packets
 expectedSeq = 1
+# Keep track of the total number of lost packets
 lostPackets = 0
+
 while True:
     try:
         # Receive the client packet along with the address it is coming from
         message, address = serverSocket.recvfrom(1024)
         seqNumBytes, timestampBytes = message.split()
+        
+        # Decode seqNum
         seqNum = int(seqNumBytes)
+        # Decode timestamp
         timestamp = float(str(timestampBytes)[2:-1])
+        
+        # Measure end time
         endTime = time.time()
+        # Calculate time difference between time sent and time received
         timeDifference = endTime - timestamp
+        
+        # Check if seqNum matches to see if packet was lost
         if expectedSeq == seqNum:
             expectedSeq += 1
         else:
             lostPackets += 1
             expectedSeq = seqNum + 1
+            
+        # The server responds
         serverSocket.sendto(message, address)
-        print('Packet received with sequence number %d\nTime difference: %f\n' % (seqNum, timeDifference))
+        print('Packet received with sequence number %d\n
+               Time difference: %f\n' % (seqNum, timeDifference))
+               
+   # The server times out, count total lost packets
     except timeout:
         print('Client application stopped.\nLost packets: %d' % lostPackets)
-    
-    # Otherwise, the server responds
