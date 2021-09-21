447fb2e51e7b7588da2fc5f6160123996b9631ab
tikagergaia <57067592+tikagergaia@users.noreply.github.com>
2021-09-20 20:59:26 -0400

    Add files via upload

--- Group3_WebClient.py
+++ Group3_WebClient.py
@@ -4,32 +4,19 @@ import sys
 Command line argument: import sys sys.argv
 sys.argv is a list,no need for split
 """
-server_host = sys.argv[1] # IP address is the first argument
-server_port = sys.argv[2] # port is the second argument
-filename = sys.argv[3] # filename is the third directory
+server_host = sys.argv[1]
+server_port = sys.argv[2]
+filename = sys.argv[3]
 
+host_port = "%s:%s" %(server_host, server_port)
 try:
 	client_socket = socket(AF_INET,SOCK_STREAM)
-	client_socket.connect((server_host,int(server_port))) # connect to server
-    # make header
- 	header = {
- 	"first_header" : "GET /%s HTTP/1.1" %(filename),
- 	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
- 	"Accept-Language": "en-us",
- 	"Host": "%s:%s" %(server_host, server_port)
- 	}
- 	http_header = "\r\n".join("%s:%s" %(item,header[item]) for item in header)
- 	print(http_header)
- 	client_socket.send("%s\r\n\r\n" %(http_header))
+	client_socket.connect((server_host,int(server_port)))
+	client_socket.send(('GET / HTTP/1.1\r\nHost:%s\r\n\r\n' %(server_host)).encode())
 
 except IOError:
-	sys.exit(1)
-final=""
-message=client_socket.recv(1024)
-# while response_message:
-# 	final += response_message
-# 	response_message = client_socket.recv(1024)
 
+	sys.exit(1)
+message=client_socket.recv(1024)
 client_socket.close()
 print (message.decode())
-sys.exit()
