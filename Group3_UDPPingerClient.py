2319a82ff112d5fed7cbf465edd510493824677d
Thomas Quintanilla <quintanillathomas00@gmail.com>
2021-09-20 20:10:39 -0400

    Updated UDP client code to include statistics

--- Group3_UDPPingerClient.py
+++ Group3_UDPPingerClient.py
@@ -10,12 +10,14 @@ Created on Sat Sep 18 14:05:58 2021
 # We will need the following modules to generate randomized lost packets
 import time
 from socket import *
+import numpy as np
 
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 clientSocket.settimeout(2.0)
 message = 'ping'
 address = ('localhost', 12000)
+times = []
 
 for seqNum in range(1,16):
     startTime = time.time()
@@ -27,5 +29,12 @@ for seqNum in range(1,16):
         print('#%d'%seqNum)
         print('Response Message:%s'%response)
         print('RTT:%fseconds'%RTT)
+        times.append(float(RTT))
     except timeout:
         print('Request timed out')
+
+print("Max time:%f" % max(times))
+print("Min time:%f" % min(times))
+print("Median time:%f" % np.median(times))
+print("Average time:%f" % np.average(times))
+print("Packet loss rate: %%%f" % ((15 - len(times))/15 * 100))
